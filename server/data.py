3b007b23bd076e6d575762358f4bf9ef45faef7f
feliperodsdev <libertyisrequired@gmail.com>
2023-01-03 13:41:33 -0300

    feat: grpc server

---
+++ server/data.py
@@ -0,0 +1,5 @@
+fakeData = [
+    ProductItem(id=1, price=75.7, name="Mesa Digitalizadora"),
+    ProductItem(id=2, price=120, name="Headset Razer"),
+    ProductItem(id=3, price=155, name="Geladeira"),
+]
