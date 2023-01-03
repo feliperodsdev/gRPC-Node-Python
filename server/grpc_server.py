6feb469587d6ac9c94c9b0906aa71f17634c4fbd
liber <libertyisrequired@gmail.com>
2023-01-03 17:15:44 -0300

    feat: finish servergrpc

--- server/grpc_server.py
+++ server/grpc_server.py
@@ -11,6 +11,20 @@ class GrpcService(protobufs.product_pb2_grpc.ProductServiceServicer):
         product = [x for x in fakeData if x.id == request.id]
         return product[0]
 
+    def Insert(self, request, context):
+        product = ProductItem(id=len(fakeData) + 1, name=request.name,
+                        price=request.price)
+        fakeData.append(product)
+        return product
+
+    def Delete(self, request, context):
+        product = [x for x in fakeData if x.id == request.id]
+        if len(product) > 0:
+            fakeData.remove(product[0])
+            return product[0]
+        else:
+            return product
+
 def server_start():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     protobufs.product_pb2_grpc.add_ProductServiceServicer_to_server(GrpcService(), server)
