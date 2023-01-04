e9073f4288279e0ac738067f2256cafec6916ba8
liber <libertyisrequired@gmail.com>
2023-01-04 16:54:13 -0300

    feat: finish

--- client/src/app.js
+++ client/src/app.js
@@ -1,5 +1,5 @@
 var express = require('express'); 
-const {deleteProduct, insertProduct, listProducts} = require('../grpc_client')
+const {deleteProduct, insertProduct, listProducts, findById} = require('../grpc_client')
 
 const app = express(); 
 
@@ -9,6 +9,29 @@ app.get('/list', async (req, res) =>
 {
     const products = await listProducts(); 
     res.status(200).send(products);
+})
+
+app.get('/find/:id', async (req, res) => 
+{
+    const product = await findById(req.params.id)
+    res.status(200).send(product)
+})
+
+app.delete('/delete/:id', async (req, res) => 
+{
+    const product = await deleteProduct(req.params.id)
+    res.status(200).send(product)
+})
+
+app.post('/insert', async (req, res) => 
+{
+    if(!req.body || req.body.name.trim() == "" ||  req.body.price == 0 || typeof req.body.price != Number) {
+        res.status(400).send({msg: 'field is required'})
+    }
+
+    const {price, name} = req.body
+    const prod = await insertProduct({price, name})
+    res.status(200).send(prod)
 })
 
 const start = () => 
