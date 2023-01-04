51d361bc5979871a6d0b38eb5c2322c8eef20eb0
feliperodsdev <libertyisrequired@gmail.com>
2023-01-04 08:35:50 -0300

    feat: promise

--- client/grpc_client.js
+++ client/grpc_client.js
@@ -1,48 +1,64 @@
-var PROTO_PATH = __dirname + '/product.proto';
-var grpc = require('@grpc/grpc-js');
-var protoLoader = require('@grpc/proto-loader');
+var PROTO_PATH = __dirname + "/product.proto";
+var grpc = require("@grpc/grpc-js");
+var protoLoader = require("@grpc/proto-loader");
 
-var packageDefinition = protoLoader.loadSync(
-    PROTO_PATH,
-    {
-        keepCase: true,
-        longs: String,
-        enums: String,
-        defaults: true,
-        oneofs: true
-    });
+var packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+});
 
 var protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
-const client = new protoDescriptor.ProductService('0.0.0.0:50051', grpc.credentials.createInsecure());
+const client = new protoDescriptor.ProductService(
+  "0.0.0.0:50051",
+  grpc.credentials.createInsecure()
+);
 
+const findById = async (id) => {
+  const prod = await new Promise((resolve, reject) => {
+    client.Find({ id: id }, function (err, response) {
+      if (err) reject(err);
+      resolve(response);
+    });
+  });
+  return prod;
+};
 
-const insertProduct = (prod) => 
-{
-    client.Insert(prod, function(err, response){
-        console.log(response)
-    })
-}
+const insertProduct = async (prod) => {
+  const prodToAdd = await new Promise((resolve, reject) => {
+    client.Insert(prod, function (err, response) {
+      if (err) reject(err);
+      resolve(response);
+    });
+  });
+  return prodToAdd;
+};
 
-const listProducts = async () => 
-{
-    const prods = await new Promise((resolve, reject)=>{
-        client.list({},function(err, response){
-          if(err) reject (err)
-          resolve(response)
-        }
-    )})
-    return prods; 
-}
+const listProducts = async () => {
+  const prods = await new Promise((resolve, reject) => {
+    client.List({}, function (err, response) {
+      if (err) reject(err);
+      resolve(response);
+    });
+  });
+  return prods;
+};
 
-const deleteProduct = (id) => 
-{
-    client.Delete({id: id}, function(err, response){
-        console.log(response)
-    })
-}
+const deleteProduct = async (id) => {
+  const prodToDelete = await new Promise((resolve, reject) => {
+    client.Delete({ id: id }, function (err, response) {
+      if (err) reject(err);
+      resolve(response);
+    });
+  });
+  return prodToDelete;
+};
 
 module.exports = {
-    deleteProduct, 
-    listProducts, 
-    insertProduct
-}
+  deleteProduct,
+  listProducts,
+  insertProduct,
+  findById,
+};
